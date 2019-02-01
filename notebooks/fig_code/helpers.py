b23e67bd84b15df2dd925ea050ec22f9b18e2440
Ian Rose <ian.r.rose@gmail.com>
2019-02-01 12:00:22 -0800

    More work on introducing skl

--- notebooks/fig_code/helpers.py
+++ notebooks/fig_code/helpers.py
@@ -2,7 +2,7 @@
 Small helpers for code that is not shown in the notebooks
 """
 
-from sklearn import neighbors, datasets, linear_model
+from sklearn import neighbors, datasets, linear_model, naive_bayes
 import pylab as pl
 import numpy as np
 from matplotlib.colors import ListedColormap
@@ -38,6 +38,35 @@ def plot_iris_knn():
     pl.axis('tight')
 
 
+    
+def plot_iris_nb():
+    iris = datasets.load_iris()
+    X = iris.data[:, :2]  # we only take the first two features. We could
+                        # avoid this ugly slicing by using a two-dim dataset
+    y = iris.target
+
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    
+    nb = naive_bayes.GaussianNB()
+    nb.fit(X, y)
+    
+    Z = nb.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    pl.figure()
+    pl.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    pl.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    pl.xlabel('sepal length (cm)')
+    pl.ylabel('sepal width (cm)')
+    pl.axis('tight')
+
+    
 def plot_polynomial_regression():
     rng = np.random.RandomState(0)
     x = 2*rng.rand(100) - 1
